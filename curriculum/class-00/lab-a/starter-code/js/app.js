fbf5f8b0919055edaf1f14eef452d98e1a1a699e
ahrenswett <ahrenswett@gmail.com>
2019-07-16 21:35:07 -0700

    Lalalalala

--- curriculum/class-00/lab-a/starter-code/js/app.js
+++ curriculum/class-00/lab-a/starter-code/js/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 const names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 
 const leftImage = document.getElementById('left');
